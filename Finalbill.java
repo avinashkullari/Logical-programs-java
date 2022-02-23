2a25a4d646cb24f30638b43b8bbe7c4ebd2a352e
avinashkullari <91649716+avinashkullari@users.noreply.github.com>
2022-02-23 15:41:04 +0530

    Add files via upload

---
+++ Finalbill.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Finalbill {
+    
+    public static void main ( String [] args ) {
+        
+        double  units=0 ,bill = 0 , gst =0 , finalbill=0;
+        
+        Scanner sc = new Scanner (System.in);
+         
+         System.out.println ("Enter the no of units");
+          units = sc.nextDouble();
+          
+          bill= units*34.56;
+          
+          gst=(bill*18)/100;
+          
+         finalbill=(bill+gst);
+         System.out.println("The Final Bill is : " +finalbill);
+    } 
+}
